81ba6c498bb82ad54f09fc78f5898c460926050f
abdelrazekAli <dev.abdelrazek@gmail.com>
2022-10-04 20:43:14 +0200

    refactor components

--- client/src/components/user/payments/PaymentWrapper.tsx
+++ client/src/components/user/payments/PaymentWrapper.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
 import { StripePayment } from "./StripePayment";
 import { Elements } from "@stripe/react-stripe-js";
