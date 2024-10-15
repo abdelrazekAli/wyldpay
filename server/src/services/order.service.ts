a74230cda7f0c91d883d5b9742402993690d8f86
abdelrazekAli <dev.abdelrazek@gmail.com>
2024-10-15 21:41:54 +0300

    Implement Redis cache for orders

--- server/src/services/order.service.ts
+++ server/src/services/order.service.ts
@@ -1,6 +1,10 @@
-import { number } from "joi";
 import OrderModel from "../models/order.model";
 import { OrderProps } from "../types/order.type";
+import {
+  deleteCachedValue,
+  getCachedValue,
+  setCacheValue,
+} from "./cache.service";
 
 // Get order by id
 export const fetchOrderById = async (orderId: string) => {
@@ -14,22 +18,41 @@ export const fetchOrdersByRestaurantID = async (
   limit: number
 ) => {
   const skip = (page - 1) * limit; // Calculate how many documents to skip for pagination
+  const cacheKey = `restaurant:${restaurantId}:orders:page:${page}`;
 
-  return (await OrderModel.find({ restId: restaurantId }, { __v: 0 })
+  // Check if the data is in cache
+  const cachedOrders = await getCachedValue(cacheKey);
+  if (cachedOrders) {
+    return JSON.parse(cachedOrders) as OrderProps[];
+  }
+
+  // If not in cache, fetch from the database
+  const orders = (await OrderModel.find({ restId: restaurantId }, { __v: 0 })
     .sort({
       createdAt: -1,
     })
     .skip(skip)
     .limit(limit)) as OrderProps[] | [];
-};
 
-// Count total orders number
-export const totalOrders = async (restaurantId: string) => {
-  return await OrderModel.countDocuments({ restId: restaurantId });
+  // Cache the items for future requests
+  await setCacheValue(cacheKey, JSON.stringify(orders));
+
+  return orders;
 };
 
 // Post new order
 export const createOrder = async (orderData: OrderProps) => {
   const newOrder = new OrderModel(orderData);
-  return await newOrder.save();
+  const order = await newOrder.save();
+
+  // Invalidate cache for orders by restaurant ID
+  const cacheKey = `restaurant:${order.restId}:orders:page:1`;
+  await deleteCachedValue(cacheKey);
+
+  return order;
+};
+
+// Count total orders number
+export const totalOrders = async (restaurantId: string) => {
+  return await OrderModel.countDocuments({ restId: restaurantId });
 };
