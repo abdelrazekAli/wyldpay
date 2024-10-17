d55c88cef7fb039cc3ca857a574aeaf28c3eb426
abdelrazekAli <dev.abdelrazek@gmail.com>
2024-10-17 21:22:20 +0300

    Refactor subscriptions service

--- server/src/controllers/subscription.controller.ts
+++ server/src/controllers/subscription.controller.ts
@@ -1,83 +1,61 @@
 import { Request, Response } from "express";
 import logger from "../config/logger.config";
 import { UserProps } from "../types/user.type";
-import { stripe } from "../config/stripe.config";
 import { findUserById } from "../services/user.service";
 import { handleClientError, handleServerError } from "../utils/error.util";
+import {
+  checkSubscriptions,
+  fetchSubscriptionPrices,
+  initiatePaymentSession,
+} from "../services/stripe.service";
 
-// Get all subscription prices from Stripe
+// Fetch subscription prices
 export const getSubscriptionPrices = async (req: Request, res: Response) => {
   try {
-    const prices = await stripe.prices.list({
-      apiKey: process.env.STRIPE_SECRET_KEY!,
-    });
-    logger.info("Fetched subscription prices from Stripe");
+    const prices = await fetchSubscriptionPrices();
     return res.json(prices.data);
   } catch (error: unknown) {
     return handleServerError(res, error, "Failed to fetch subscription prices");
   }
 };
 
-// Create Stripe payment session
+// Create a payment session for a user
 export const createPaymentSession = async (req: Request, res: Response) => {
-  const { userId } = req.body;
+  const { userId, priceId } = req.body;
 
   try {
     const user = (await findUserById(userId)) as UserProps;
-    if (!user)
+    if (!user) {
       return handleClientError(res, `User with id ${userId} not found`, 404);
+    }
 
-    const session = await stripe.checkout.sessions.create(
-      {
-        mode: "subscription",
-        payment_method_types: ["card"],
-        line_items: [
-          {
-            price: req.body.priceId,
-            quantity: 1,
-          },
-        ],
-        success_url: `${process.env.BASE_URL}/admin/subscription-success`,
-        cancel_url: `${process.env.BASE_URL}/admin/subscription-failed`,
-        customer: user.stripeCustomerId,
-      },
-      {
-        apiKey: process.env.STRIPE_SECRET_KEY!,
-      }
+    const session = await initiatePaymentSession(
+      priceId,
+      user.stripeCustomerId
     );
 
-    logger.info(`Created Stripe session for user: ${user._id}`);
+    logger.info(`Created payment session for user: ${user._id}`);
     return res.json(session);
   } catch (error: unknown) {
     return handleServerError(
       res,
       error,
-      `Failed to create Stripe payment session`
+      "Failed to create Stripe payment session"
     );
   }
 };
 
-// Check user subscription
+// Check the subscription status for a user
 export const checkUserSubscription = async (req: Request, res: Response) => {
   const { userId } = req.body;
 
   try {
     const user = (await findUserById(userId)) as UserProps;
-    if (!user)
+    if (!user) {
       return handleClientError(res, `User with id ${userId} not found`, 404);
+    }
 
-    const subscriptions = await stripe.subscriptions.list(
-      {
-        customer: user.stripeCustomerId,
-        status: "all",
-        expand: ["data.default_payment_method"],
-      },
-      {
-        apiKey: process.env.STRIPE_SECRET_KEY!,
-      }
-    );
-
-    logger.info(`Fetched subscriptions for user: ${user._id}`);
+    const subscriptions = await checkSubscriptions(user.stripeCustomerId);
     return res.json(subscriptions);
   } catch (error: unknown) {
     return handleServerError(res, error, "Failed to check user subscription");
--- server/src/services/stripe.service.ts
+++ server/src/services/stripe.service.ts
@@ -1,9 +1,40 @@
 import { stripe } from "../config/stripe.config";
 
+const { BASE_URL, STRIPE_SECRET_KEY } = process.env;
+
 // Create a new Stripe customer
 export const createStripeCustomer = async (email: string): Promise<any> => {
   return await stripe.customers.create(
     { email },
-    { apiKey: process.env.STRIPE_SECRET_KEY }
+    { apiKey: STRIPE_SECRET_KEY }
   );
+};
+
+// Fetch Stripe subscription prices
+export const fetchSubscriptionPrices = async () => {
+  return await stripe.prices.list({ apiKey: STRIPE_SECRET_KEY });
+};
+
+// Create Stripe payment session
+export const initiatePaymentSession = async (
+  priceId: string,
+  stripeCustomerId: string
+) => {
+  return await stripe.checkout.sessions.create({
+    mode: "subscription",
+    payment_method_types: ["card"],
+    line_items: [{ price: priceId, quantity: 1 }],
+    success_url: `${BASE_URL}/admin/subscription-success`,
+    cancel_url: `${BASE_URL}/admin/subscription-failed`,
+    customer: stripeCustomerId,
+  });
+};
+
+// Check Stripe user subscription
+export const checkSubscriptions = async (stripeCustomerId: string) => {
+  return await stripe.subscriptions.list({
+    customer: stripeCustomerId,
+    status: "all",
+    expand: ["data.default_payment_method"],
+  });
 };
