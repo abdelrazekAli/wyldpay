2798a68bc79647b62d123cffede55ba7b8396ed0
abdelrazekAli <dev.abdelrazek@gmail.com>
2024-10-23 15:23:17 +0300

    Add in-memory MongoDB for testing

--- server/__tests__/bootstrap.ts
+++ server/__tests__/bootstrap.ts
@@ -1,13 +1,26 @@
 const mongoose = require("mongoose");
-import connectToDatabase from "../src/config/db.config";
+import { MongoMemoryServer } from "mongodb-memory-server";
 import { connectToRedis } from "../src/config/redis.config";
 
+let mongoServer: MongoMemoryServer;
+
 beforeAll(async () => {
-  await connectToDatabase();
+  // Create an in-memory MongoDB server
+  mongoServer = await MongoMemoryServer.create();
+
+  // Connect to the in-memory MongoDB
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri);
+
+  // connect to Redis
   await connectToRedis();
+
+  // Drop the database to ensure a clean state
   await mongoose.connection.dropDatabase();
 });
 
 afterAll(async () => {
+  // Clean up the connections and stop the MongoDB server
   await mongoose.disconnect();
+  await mongoServer.stop();
 });
