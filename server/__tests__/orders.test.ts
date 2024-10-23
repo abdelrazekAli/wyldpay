f06a8b3d80874bb1be5fdd47258418d721eaeccc
abdelrazekAli <dev.abdelrazek@gmail.com>
2024-10-23 06:07:55 +0300

    Refactor order API tests

--- server/__tests__/orders.test.ts
+++ server/__tests__/orders.test.ts
@@ -1,5 +1,5 @@
-import app from "../src/server";
 import request from "supertest";
+import app from "../src/server";
 import { testUserId } from "./fakeData/user.data";
 import { generateToken } from "../src/utils/token.util";
 import { invalidOrderData, orderData } from "./fakeData/order.data";
@@ -12,56 +12,61 @@ const agent = request.agent(app);
 let testOrderId: string;
 const accessToken = generateToken({ _id: testUserId }, "1h");
 
-// Test suite for order routes
+// Test Order API
 describe("Order API Tests", () => {
-  // Test creating a new order
-  it("should create a new order", async () => {
-    const response = await agent
-      .post(`${API_VERSION}/orders`)
-      .set("auth-token", accessToken)
-      .send(orderData);
+  describe("Post /orders", () => {
+    it("should create a new order", async () => {
+      const response = await agent
+        .post(`${API_VERSION}/orders`)
+        .set("auth-token", accessToken)
+        .send(orderData);
 
-    expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty("_id");
-    testOrderId = response.body._id;
+      expect(response.status).toBe(201);
+      expect(response.body).toHaveProperty("_id");
+      testOrderId = response.body._id;
+    });
+
+    it("should return validation error if order data is invalid", async () => {
+      const response = await agent
+        .post(`${API_VERSION}/orders`)
+        .set("auth-token", accessToken)
+        .send(invalidOrderData);
+
+      expect(response.status).toBe(400);
+    });
   });
 
-  // Test fetching an order by ID
-  it("should fetch an order by ID", async () => {
-    const response = await agent.get(`${API_VERSION}/orders/${testOrderId}`);
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty("_id", testOrderId);
+  describe("Get /orders/:id", () => {
+    it("should fetch an order by ID", async () => {
+      const response = await agent.get(`${API_VERSION}/orders/${testOrderId}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("_id", testOrderId);
+    });
+
+    it("should return 404 if order not found", async () => {
+      const invalidOrderId = "670432a5fe5547ad58b8e762";
+      const response = await agent.get(
+        `${API_VERSION}/orders/${invalidOrderId}`
+      );
+
+      expect(response.status).toBe(404);
+      expect(response.body).toBe(`Order not found with id: ${invalidOrderId}`);
+    });
   });
 
-  // Test getting orders for a restaurant
-  it("should fetch orders by restaurant id", async () => {
-    const page = 1;
-    const limit = 10;
-    const response = await agent
-      .get(`${API_VERSION}/orders?page=${page}&limit=${limit}`)
-      .set("auth-token", accessToken);
+  describe("Get /orders", () => {
+    it("should fetch orders by restaurant id", async () => {
+      const page = 1;
+      const limit = 10;
+      const response = await agent
+        .get(`${API_VERSION}/orders?page=${page}&limit=${limit}`)
+        .set("auth-token", accessToken);
 
-    expect(response.status).toBe(200);
-    expect(Array.isArray(response.body.orders)).toBe(true);
-    expect(response.body).toHaveProperty("orders");
-    expect(response.body).toHaveProperty("totalPages");
-  });
-
-  // Test handling not found for an order by ID
-  it("should return 404 if order not found", async () => {
-    const invalidOrderId = "670432a5fe5547ad58b8e762";
-    const response = await agent.get(`${API_VERSION}/orders/${invalidOrderId}`);
-
-    expect(response.status).toBe(404);
-    expect(response.body).toBe(`Order not found with id: ${invalidOrderId}`);
-  });
-
-  // Test validation error when creating an order with invalid data
-  it("should return validation error if order data is invalid", async () => {
-    const response = await agent
-      .post(`${API_VERSION}/orders`)
-      .set("auth-token", accessToken)
-      .send(invalidOrderData);
-    expect(response.status).toBe(400);
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.body.orders)).toBe(true);
+      expect(response.body).toHaveProperty("orders");
+      expect(response.body).toHaveProperty("totalPages");
+    });
   });
 });
