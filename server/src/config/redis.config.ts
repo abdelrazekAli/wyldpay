56ff6ffaee3ba6123208c9a34522370142324e0b
abdelrazekAli <dev.abdelrazek@gmail.com>
2024-10-23 15:20:42 +0300

    Add redis-mock for testing

--- server/src/config/redis.config.ts
+++ server/src/config/redis.config.ts
@@ -1,12 +1,20 @@
 import { createClient } from "redis";
 import logger from "./logger.config";
 
-const redisClient = createClient({ url: process.env.REDIS_URL });
+const isTestEnv = process.env.NODE_ENV === "test";
+const redisClient = isTestEnv
+  ? require("redis-mock").createClient() // Use redis-mock in test environment
+  : createClient({ url: process.env.REDIS_URL }); // Real Redis client in other environments
 
 export const connectToRedis = async () => {
   try {
-    await redisClient.connect();
-    logger.info("Connected to Redis");
+    if (!isTestEnv) {
+      // Only connect if not in the test environment
+      await redisClient.connect();
+      logger.info("Connected to Redis");
+    } else {
+      logger.info("Using mock Redis client in test environment");
+    }
   } catch (err) {
     logger.error("Could not connect to Redis cache", err);
   }
