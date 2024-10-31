6d687a6c7005582b36ae3ee15d19f5fcbeda1884
abdelrazekAli <dev.abdelrazek@gmail.com>
2024-10-31 18:26:26 +0300

    implement Singleton pattern for DB connection

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -1,14 +1,30 @@
 import mongoose from "mongoose";
 import logger from "./logger.config";
 
-const connectToDatabase = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL);
-    logger.info("DB connected");
-  } catch (error: any) {
-    logger.error("Could not connect to DB", error.message);
-    process.exit(1);
-  }
-};
+const Database = (() => {
+  const connectToDatabase = async () => {
+    /*
+     * mongoose.connection.readyState checks the status of mongoose connection
+     * it returns 1 if Connected and 0 if Disconnected
+     */
+    if (mongoose.connection.readyState === 1) {
+      logger.info("DB is already connected, reusing the existing connection.");
+      return mongoose.connection;
+    }
 
-export default connectToDatabase;
+    try {
+      await mongoose.connect(process.env.DB_URL as string);
+      logger.info("DB connected");
+      return mongoose.connection;
+    } catch (error: any) {
+      logger.error("Could not connect to DB", error.message);
+      process.exit(1);
+    }
+  };
+
+  return {
+    getInstance: connectToDatabase,
+  };
+})();
+
+export default Database.getInstance;
