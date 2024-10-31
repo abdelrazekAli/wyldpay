d9ca741328410c15ed5ed8f51c9f2fd1f58b4e7e
abdelrazekAli <dev.abdelrazek@gmail.com>
2024-10-31 19:38:44 +0300

    Implement redis-mock for testing

--- server/__tests__/bootstrap.ts
+++ server/__tests__/bootstrap.ts
@@ -1,26 +1,34 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
-import { connectToRedis } from "../src/config/redis.config";
 
 let mongoServer: MongoMemoryServer;
 
 beforeAll(async () => {
-  // Create an in-memory MongoDB server
-  mongoServer = await MongoMemoryServer.create();
+  try {
+    // Create an in-memory MongoDB server
+    mongoServer = await MongoMemoryServer.create();
 
-  // Connect to the in-memory MongoDB
-  const mongoUri = mongoServer.getUri();
-  await mongoose.connect(mongoUri);
+    // Connect to the in-memory MongoDB
+    const mongoUri = mongoServer.getUri();
+    await mongoose.connect(mongoUri);
 
-  // connect to Redis
-  await connectToRedis();
-
-  // Drop the database to ensure a clean state
-  await mongoose.connection.dropDatabase();
+    // Drop the database to ensure a clean state
+    await mongoose.connection.dropDatabase();
+  } catch (error) {
+    console.error("Error during test setup:", error);
+    throw error; // Rethrow to fail the test setup if needed
+  }
 });
 
 afterAll(async () => {
-  // Clean up the connections and stop the MongoDB server
-  await mongoose.disconnect();
-  await mongoServer.stop();
+  try {
+    // Clean up the connections and stop the MongoDB server
+    await mongoose.disconnect();
+    await mongoServer.stop();
+
+    // Optionally, if you have a disconnect function for Redis
+    // await disconnectFromRedis(); // Ensure you implement this in your Redis config
+  } catch (error) {
+    console.error("Error during test teardown:", error);
+  }
 });
